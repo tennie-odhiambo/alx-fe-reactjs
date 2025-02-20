f1e345a7054beb3d3f67ebe7b532eba6085d28c8
unknown <tennie-odhiambo>
2025-02-20 11:40:17 +0300

    Adding style to myreact

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ padding: '20px', backgroundColor: '#f4f4f4', textAlign: 'center' }}>
+            <p style={{ fontSize: '18px', lineHeight: '1.6' }}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
