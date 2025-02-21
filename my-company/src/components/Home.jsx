03159c56c266996469cedfcd47cf3dc118841bd8
unknown <tennie-odhiambo>
2025-02-21 23:16:51 +0300

    Set up routing in App.jsx for homepage, about, services, and contact pages

---
+++ my-company/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return (
+      <div style={{ padding: '20px', backgroundColor: '#ECF0F1',}}>
+        <h1 style={{color: '#2C3E50'}}>Welcome to Our Company</h1>
+        <p style={{ color: '#34495E' }}>We are dedicated to delivering excellence in all our services.</p>
+      </div>
+    );
+  }
+
+  export default Home;
+
---
+++ my-company/src/components/Services.jsx
@@ -0,0 +1,14 @@
+function Services() {
+    return (
+      <div style={{ padding: '20px', backgroundColor: '#ECF0F1' }}>
+        <h1 style={{ color: '#2C3E50' }} >Our Services</h1>
+        <ul style={{ color: '#34495E' }}>
+          <li>Technology Consulting</li>
+          <li>Market Analysis</li>
+          <li>Product Development</li>
+        </ul>
+      </div>
+    );
+  }
+
+  export default Services;
