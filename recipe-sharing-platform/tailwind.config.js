25b447d5655f3a5c05739da29f162451dd7f6fa1
unknown <tennie-odhiambo>
2025-03-19 09:21:52 +0300

    Creating react and installing tailwind.css

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,8 @@
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
